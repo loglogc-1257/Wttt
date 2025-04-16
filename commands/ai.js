6627435ec559e47a243d217685edeff80a85defa
loglogc-1257 <fadonougbosebastien7@gmail.com>
2025-04-16 07:54:58 +0100

    Update ai.js

--- commands/ai.js
+++ commands/ai.js
@@ -3,7 +3,7 @@ const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
   name: 'ai',
-  description: 'Interact with Brave-AI',
+  description: 'Interact with Voilà AI',
   usage: 'gpt4 [your message]',
   author: 'coffee',
 
@@ -16,7 +16,7 @@ module.exports = {
     }
 
     try {
-      const { data } = await axios.get(`https://kaiz-apis.gleeze.com/api/brave-ai?ask=${encodeURIComponent(prompt)}`);
+      const { data } = await axios.get(`https://kaiz-apis.gleeze.com/api/voila?ask=${encodeURIComponent(prompt)}`);
       const response = data.response;
 
       const parts = [];
