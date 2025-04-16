0cabe455202746321bfcdd61e0e88d6a725765c7
loglogc-1257 <fadonougbosebastien7@gmail.com>
2025-04-16 07:26:09 +0100

    Update ai.js

--- commands/ai.js
+++ commands/ai.js
@@ -3,27 +3,32 @@ const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
   name: 'ai',
-  description: 'Interact with GPT-4o',
+  description: 'Interact with Brave-AI',
   usage: 'gpt4 [your message]',
   author: 'coffee',
 
   async execute(senderId, args, pageAccessToken) {
     const prompt = args.join(' ');
-    if (!prompt) return sendMessage(senderId, { text: "Veuillez poser votre question ou tapez 'help' pour voir les autres commandes disponibles." }, pageAccessToken);
+    if (!prompt) {
+      return sendMessage(senderId, {
+        text: "Veuillez poser votre question ou tapez 'help' pour voir les autres commandes disponibles."
+      }, pageAccessToken);
+    }
 
     try {
-      const { data: { response } } = await axios.get(`https://kaiz-apis.gleeze.com/api/gpt-4o-pro?ask=${encodeURIComponent(prompt)}&uid=${senderId}`);
-      const parts = [];
+      const { data } = await axios.get(`https://kaiz-apis.gleeze.com/api/brave-ai?ask=${encodeURIComponent(prompt)}`);
+      const response = data.response;
 
+      const parts = [];
       for (let i = 0; i < response.length; i += 1800) {
         parts.push(response.substring(i, i + 1800));
       }
-      
+
       for (const part of parts) {
         await sendMessage(senderId, { text: part }, pageAccessToken);
       }
     } catch {
-      sendMessage(senderId, { 
+      sendMessage(senderId, {
         text: "🤖 Oups ! Une petite erreur est survenue.\n\n" +
               "❓ Veuillez poser votre question ou tapez 'help' pour voir les autres commandes disponibles."
       }, pageAccessToken);
