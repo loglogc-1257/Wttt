367cbd378cf380d5b252a0d09212d89627140e19
loglogc-1257 <fadonougbosebastien7@gmail.com>
2025-04-18 15:02:26 +0100

    Create link.js

---
+++ commands/link.js
@@ -0,0 +1,22 @@
+const { sendMessage } = require('../handles/sendMessage');
+const fs = require('fs');
+
+const token = fs.readFileSync('token.txt', 'utf8').trim();
+
+module.exports = {
+  name: 'link',
+  description: 'Envoie un lien spécifique.',
+  usage: 'link',
+  author: 'Stanley',
+
+  async execute(senderId) {
+    try {
+      await sendMessage(senderId, {
+        text: 'Voici ton lien :\nhttps://redi-12dy.onrender.com/'
+      }, token);
+    } catch (error) {
+      console.error(error);
+      await sendMessage(senderId, { text: '❌ Impossible d\'envoyer le lien.' }, token);
+    }
+  }
+};
